5764a276a28ef132e27011efdb64dbe0c0bcc42b
Dawid Gardziński <dawidgardzinski@gmail.com>
2023-03-11 18:20:57 +0100

    handle arrow entry button

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import "./styles/normalize.css";
 import "./styles/style.css";
 import "./styles/tablet.css";
 import "./styles/mobile.css";
+import "./styles/utils.css";
 import { uncheckHamburgerCheckboxOnMenuItemClick } from "./scripts/uncheckHamburgerCheckboxOnMenuItemClick";
 import { clickOutsideCallback } from "./scripts/clickOutsideCallback";
+
+document.querySelectorAll(".js-disabled").forEach((element) => {
+  element.classList.remove("js-disabled");
+});
 
 const checkboxHamburgerElement = document.getElementById(
   "hamburgerCheckbox"
@@ -20,4 +25,12 @@ document.querySelectorAll(".nav-list__item").forEach((element) => {
 
 clickOutsideCallback(mobileMenu, () => {
   uncheckHamburgerCheckboxOnMenuItemClick(checkboxHamburgerElement);
+});
+
+const startButton = document.getElementById("startButton");
+const aboutMeSection = document.getElementById("about");
+
+startButton?.addEventListener("click", () => {
+  if (!aboutMeSection) return;
+  aboutMeSection.scrollIntoView();
 });
